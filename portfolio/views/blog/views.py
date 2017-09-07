0c91b30217c3a67a4db71bb960c9acb46f83a586
uhocaogl <umut.hocaolgu@meredith.com>
2017-09-06 22:38:16 -0500

    Create add post function

--- portfolio/views/blog/views.py
+++ portfolio/views/blog/views.py
@@ -1,4 +1,7 @@
-from flask import render_template, Blueprint, request, flash, redirect, url_for
+import os
+import traceback
+
+from flask import render_template, Blueprint, request, redirect, url_for
 from portfolio.views.blog.forms import AddPostForm
 from portfolio import flatpages
 from portfolio import app
@@ -8,10 +11,21 @@ blog_blueprint = Blueprint('blog', __name__)
 POST_DIR = app.config['POST_DIR']
 
 
+def save_post(title, body):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    file_name = str(title) + '.md'
+    write_path = os.path.join(basedir, 'content', 'posts', file_name)
+    print('write path: ' + write_path)
+
+    with open(write_path, 'w') as f:
+        f.write('title: ' + title + '\n')
+        f.write(body)
+
+
 @blog_blueprint.route('/blogs', methods=['GET'])
 def main():
     posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
-    posts.sort(key=lambda item: item['date'], reverse=False)
+    posts.sort(key=lambda item: item['date'], reverse=True)
     return render_template('blogs.html', posts=posts)
 
 
@@ -27,6 +41,14 @@ def add_post():
     error = None
     form = AddPostForm()
     if request.method == 'POST' and form.validate_on_submit():
-        return redirect(url_for('add_post'))
+        print('Received new post data')
+        blog_title = form.blog_title.data
+        blog_body = form.blog_body.data
+        print('blog body:' + blog_body)
+        try:
+            save_post(blog_title, blog_body)
+        except Exception as e:
+            traceback.print_exc()
+        return redirect(url_for('blog.add_post'))
 
     return render_template('add_post.html', form=form, error=error)
--- portfolio/views/main/views.py
+++ portfolio/views/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, Blueprint
+
 main_blueprint = Blueprint('main', __name__)
 
 
 @main_blueprint.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
-
