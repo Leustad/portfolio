c83eeeb733dd5d3db33b799a49ba5c99cd73e2bf
uhocaogl <umut.hocaolgu@meredith.com>
2017-09-24 11:15:43 -0500

    Delete unnecessary packages

--- portfolio/views/blog/views.py
+++ portfolio/views/blog/views.py
@@ -2,8 +2,6 @@ import traceback
 from functools import wraps
 
 from flask import render_template, Blueprint, request, redirect, url_for, session, flash
-from bs4 import BeautifulSoup
-import bleach
 
 from portfolio.views.blog.forms import AddPostForm, LoginForm
 from portfolio import app, bcrypt
@@ -13,31 +11,6 @@ blog_blueprint = Blueprint('blog', __name__)
 
 usrname = app.config['USRNAME']
 smth = app.config['SMTH']
-
-VALID_TAGS = ['a', 'abbr', 'acronym', 'address', 'area', 'b', 'big',
-              'blockquote', 'br', 'button', 'caption', 'center', 'cite', 'code', 'col',
-              'colgroup', 'dd', 'del', 'dfn', 'dir', 'div', 'dl', 'dt', 'em',
-              'font', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img',
-              'ins', 'kbd', 'label', 'legend', 'li', 'map', 'menu', 'ol',
-              'p', 'pre', 'q', 's', 'samp', 'small', 'span', 'strike',
-              'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th',
-              'thead', 'tr', 'tt', 'u', 'ul', 'var', 'iframe', 'frameborder', 'allowfullscreen']
-
-VALID_ATTRIBUTES = ['abbr', 'accept', 'accept-charset', 'accesskey',
-                    'action', 'align', 'alt', 'axis', 'border', 'cellpadding', 'cellspacing',
-                    'char', 'charoff', 'charset', 'checked', 'cite', 'clear', 'cols',
-                    'colspan', 'color', 'compact', 'coords', 'datetime', 'dir',
-                    'enctype', 'for', 'headers', 'height', 'href', 'hreflang', 'hspace',
-                    'id', 'ismap', 'label', 'lang', 'longdesc', 'maxlength', 'method',
-                    'multiple', 'name', 'nohref', 'noshade', 'nowrap', 'prompt',
-                    'rel', 'rev', 'rows', 'rowspan', 'rules', 'scope', 'shape', 'size',
-                    'span', 'src', 'start', 'summary', 'tabindex', 'target', 'title', 'type',
-                    'usemap', 'valign', 'value', 'vspace', 'width']
-
-
-def sanitize_html(value):
-    soup = BeautifulSoup(bleach.clean(value, tags=VALID_TAGS, attributes=VALID_ATTRIBUTES), "html5lib")
-    return soup.renderContents()
 
 
 def login_required(test):
@@ -64,7 +37,7 @@ def post(name):
     post_title, post_body = aws_func.get_single_post(name)
     post_body = post_body.split('\r\n')
     for line in post_body:
-        content.append(str(sanitize_html(line)))
+        content.append(line)
     return render_template('blog.html', post_title=post_title, post_body=content)
 
 
@@ -112,7 +85,8 @@ def logout():
 @blog_blueprint.route('/edit/<title>', methods=['GET', 'POST'])
 @login_required
 def edit(title):
-    # TODO: S3 file manipulation can only be done with deleting and adding the new file.
+    # TODO: S3 file manipulation can only be done with deleting and adding the
+    # new file.
 
     # path = '{}/{}'.format(POST_DIR, title)
     # # post = flatpages.get_or_404(path)
